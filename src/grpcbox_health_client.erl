11c7c7f9063df3a4aecf35b01e64898ca394a99a
Calancea Daniel <d4no0007@gmail.com>
2022-06-23 16:23:20 +0300

    Generated files with the updated plugin

--- src/grpcbox_health_client.erl
+++ src/grpcbox_health_client.erl
@@ -3,7 +3,7 @@
 %% @end
 %%%-------------------------------------------------------------------
 
-%% this module was generated on 2019-03-09T00:28:46+00:00 and should not be modified manually
+%% this module was generated and should not be modified manually
 
 -module(grpcbox_health_client).
 
@@ -25,37 +25,37 @@
 
 %% @doc Unary RPC
 -spec check(grpcbox_health_pb:health_check_request()) ->
-    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 check(Input) ->
     check(ctx:new(), Input, #{}).
 
 -spec check(ctx:t() | grpcbox_health_pb:health_check_request(), grpcbox_health_pb:health_check_request() | grpcbox_client:options()) ->
-    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 check(Ctx, Input) when ?is_ctx(Ctx) ->
     check(Ctx, Input, #{});
 check(Input, Options) ->
     check(ctx:new(), Input, Options).
 
 -spec check(ctx:t(), grpcbox_health_pb:health_check_request(), grpcbox_client:options()) ->
-    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_health_pb:health_check_response(), grpcbox:metadata()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 check(Ctx, Input, Options) ->
     grpcbox_client:unary(Ctx, <<"/grpc.health.v1.Health/Check">>, Input, ?DEF(health_check_request, health_check_response, <<"grpc.health.v1.HealthCheckRequest">>), Options).
 
 %% @doc 
 -spec watch(grpcbox_health_pb:health_check_request()) ->
-    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 watch(Input) ->
     watch(ctx:new(), Input, #{}).
 
 -spec watch(ctx:t() | grpcbox_health_pb:health_check_request(), grpcbox_health_pb:health_check_request() | grpcbox_client:options()) ->
-    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 watch(Ctx, Input) when ?is_ctx(Ctx) ->
     watch(Ctx, Input, #{});
 watch(Input, Options) ->
     watch(ctx:new(), Input, Options).
 
 -spec watch(ctx:t(), grpcbox_health_pb:health_check_request(), grpcbox_client:options()) ->
-    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response().
+    {ok, grpcbox_client:stream()} | grpcbox_stream:grpc_error_response() | {error, any()}.
 watch(Ctx, Input, Options) ->
     grpcbox_client:stream(Ctx, <<"/grpc.health.v1.Health/Watch">>, Input, ?DEF(health_check_request, health_check_response, <<"grpc.health.v1.HealthCheckRequest">>), Options).
 
--- src/grpcbox_reflection_bhvr.erl
+++ src/grpcbox_reflection_bhvr.erl
@@ -3,11 +3,11 @@
 %% @end
 %%%-------------------------------------------------------------------
 
-%% this module was generated on 2019-03-09T00:28:46+00:00 and should not be modified manually
+%% this module was generated and should not be modified manually
 
 -module(grpcbox_reflection_bhvr).
 
-%% @doc 
+%% 
 -callback server_reflection_info(reference(), grpcbox_stream:t()) ->
     ok | grpcbox_stream:grpc_error_response().
 
